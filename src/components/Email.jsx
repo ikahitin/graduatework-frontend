6552f40db960c12682ba8799a1605415467e74dd
Ivan Kahitin <wiglegpro@gmail.com>
2022-07-01 10:52:13 +0300

    Push leftovers

--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -1,15 +1,30 @@
 import React from "react";
+import API from "../api";
 
 export default function Email() {
+    function sendEmail(e){
+        e.preventDefault()
+        const form = e.target;
+        const email = form.email.value
+        const data = {"email": email}
+        API({
+            method: 'post',
+            url: 'subscribe-email',
+            data: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    }
     return(
         <div className="email">
             <span className="heading">Збережіть ваш час!</span>
             <p className="subheading">Підпишіться, і ми надішлемо вам найкращі пропозиції</p>
             <div className="email-block">
                 <img src={`${process.env.PUBLIC_URL}/email-bg.svg`} alt="email-background" className="email-background"/>
-                <form action="" className="email-form">
+                <form action="" className="email-form" onSubmit={sendEmail}>
                     <div className="input-group">
-                        <input type="text" className="form-control with-icon email-icon" id="emailSubscription"
+                        <input type="email" name="email" className="form-control with-icon email-icon" id="emailSubscription"
                                placeholder="Ваш e-mail"/>
                     </div>
                     <div className="col-auto">
